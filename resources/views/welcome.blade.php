59d884484d5dcd1e8f8a38d8d6e8508a827f9b14
Yeet <107809331+y3eet@users.noreply.github.com>
2025-02-20 21:22:46 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,6 @@
-<!doctype html>
-<html data-theme="cupcake">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
-</head>
-
-<body>
+<x-layout>
     <h1 class="text-3xl font-bold underline">
         Hello world!
     </h1>
     <button class="btn btn-primary">Button</button>
-</body>
-
-</html>
+</x-layout>
