c6eaa93650ec83f71f7e47074f89346da7053764
Yeet <107809331+y3eet@users.noreply.github.com>
2025-02-20 15:37:53 +0800

    added typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ export default {
         },
     },
 
-    plugins: [require("daisyui")],
+    plugins: [require("@tailwindcss/typography"), require("daisyui")],
     daisyui: {
         themes: [
             "light",
