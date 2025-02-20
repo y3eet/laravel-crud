b623d01b2620309278d95dc4837b676c44ff5207
Yeet <107809331+y3eet@users.noreply.github.com>
2025-02-20 12:35:46 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [require("daisyui")], // Add DaisyUI here
+    daisyui: {
+        themes: ["light", "dark", "cupcake"], // Add DaisyUI themes here
+    },
+};
