7211798d51becf4c8b66f031b853aa739522dadd
Yeet <107809331+y3eet@users.noreply.github.com>
2025-02-20 21:22:39 +0800

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,13 +1,15 @@
 <x-layout>
     <div class="flex justify-center items-center min-h-[70vh]">
-        <form action="">
+        <form action="{{ route('login') }}" method="POST">
+            @csrf
             <div class="card bg-neutral text-neutral-content w-96">
                 <div class="card-body flex flex-col gap-5">
                     <h1 class="card-title">Login</h1>
-                    <input required type="email" placeholder="Email" id="email" class="input" />
-                    <input type="password" placeholder="Password" id="password" class="input" />
+                    <input required type="email" placeholder="Email" id="email" name="email" class="input"
+                        value="{{ old('email') }}" />
+                    <input type="password" placeholder="Password" id="password" name="password" class="input" />
                     <button type="submit" class="btn btn-primary">Login</button>
-                    <a href="register">Don't Have an Accoount?</a>
+                    <a href="{{ route('show.register') }}">Don't Have an Accoount?</a>
                 </div>
             </div>
         </form>
