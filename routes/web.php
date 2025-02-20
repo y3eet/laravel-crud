2c0fffb3bdc6b64507c64021b5015c78a68a1d52
Yeet <107809331+y3eet@users.noreply.github.com>
2025-02-20 21:23:23 +0800

    added auth controller & middleware

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,28 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/register', [AuthController::class, 'showRegister'])->name('show.register');
+Route::get('/login', [AuthController::class, 'showLogin'])->name('show.login');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/', function (Request $request) {
+    $user = $request->user();
+    if ($user) {
+        return redirect()->route('posts.index');
+    }
+    return redirect()->route('login');
 });
 
-Route::get('/posts', function () {
-    return view('posts.index');
-});
-
-Route::get('/login', function () {
-    return view('login');
-});
-
-Route::get('/register', function () {
-    return view('register');
+Route::middleware('auth')->group(function () {
+    Route::get('/posts', function () {
+        return view('posts.index');
+    })->name('posts.index');
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
