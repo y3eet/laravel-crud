0a1ec0820d700ab00862ca715fed4fb2a0571e05
Yeet <107809331+y3eet@users.noreply.github.com>
2025-02-20 15:30:08 +0800

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('posts', function () {
+Route::get('/posts', function () {
     return view('posts.index');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
 });
