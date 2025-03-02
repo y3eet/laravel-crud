41fba2c0a9f6e0a4fa7635879ba788e5bfcb98c2
Yeet <107809331+y3eet@users.noreply.github.com>
2025-03-02 09:49:33 +0800

    included js

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -43,7 +43,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel Crud</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
         crossorigin="anonymous"></script>
 </head>
